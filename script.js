03fe28fab5b474b40d2b90f19e1a121d9a5bfb03
DallasGiles <105821196+DallasGiles@users.noreply.github.com>
2024-12-04 13:14:43 -0700

    changed api link to render link

--- script.js
+++ script.js
@@ -14,10 +14,11 @@ document.getElementById("uploadForm").addEventListener("submit", async (event) =
     resultDiv.textContent = "Detecting...";
   
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://shape-detector-pvlg.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
+  
   
       if (!response.ok) {
         throw new Error("Failed to detect shape.");
